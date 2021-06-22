8c67a6b649b893afeb0822d0c02dd9901b59442c
yashpatidar000 <yashpatidar08@gmail.com>
2021-06-22 21:25:26 +0530

    auth system

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')
-const { collection } = require('./app/models/menu')
+//const { collection } = require('./app/models/menu')
+const passport = require('passport')
 
 // Data connection
 const url = 'mongodb://localhost/pizza';
@@ -24,6 +25,7 @@ connection.once('open', () => {
 }).catch(err => {
     console.log('Connection failed...')
 });
+
 
 //Session store
 // let mongoStore = new MongoDbStore({
@@ -43,16 +45,24 @@ app.use(session({
   cookie: { maxAge: 1000 * 60 * 60 * 24 }
 }))
 
+
+// Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(flash())
 
 //Assets
 app.use(express.static('public'))
-
+app.use(express.urlencoded({ extended: false}))
 app.use(express.json())
 
 // Global middleware
 app.use((req, res, next) => {
          res.locals.session = req.session
+         res.locals.user = req.user
          next()
 })
 
