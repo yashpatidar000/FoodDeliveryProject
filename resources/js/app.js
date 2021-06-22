04ed330ede161537721a6046bc65acd5c0af44a0
yashpatidar000 <yashpatidar08@gmail.com>
2021-06-22 10:44:06 +0530

    add to cart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,35 @@
-console.log('Hello from app jss')
+import axios from 'axios' 
+import Noty from 'noty'
+
+let addToCart = document.querySelectorAll('.add-to-cart')
+let cartCounter = document.querySelector('#cartCounter')
+
+function updateCart(pizza){
+           axios.post('/update-cart', pizza).then(res =>{
+               cartCounter.innerText = res.data.totalQty
+               new Noty({
+                   type: 'success',
+                   timeout: 100,
+                text: "Item added to cart",
+                progressBar: false,
+                // layout: 'topLeft'
+              }).show();
+           }).catch(err =>{
+            new Noty({
+                type: 'error',
+                timeout: 1000,
+             text: "Something went wrong",
+             progressBar: false,
+             // layout: 'topLeft'
+           }).show();
+
+           })
+}
+
+addToCart.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+           
+           let pizza = JSON.parse(btn.dataset.pizza)
+           updateCart(pizza)
+    })
+})
